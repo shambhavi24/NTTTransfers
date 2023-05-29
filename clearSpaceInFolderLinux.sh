f1f04b98f7fef4c293406ce9236d4c6d2d3ccf93
shambhavi24 <shambhavi.shivan@yahoo.com>
2023-05-29 05:55:48 -0400

    Add files via upload

---
+++ clearSpaceInFolderLinux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+folder_path=$1
+
+# Check if the folder exists
+if [ -d "$folder_path" ]; then
+# Delete all files in the folder sudo 
+sudo rm -rf "$folder_path"/*
+
+# Delete all subfolders in the folder 
+sudo rm -rf "$folder_path"/*/
+
+echo "Contents deleted successfully."
+else
+echo "Folder does not exist."
+fi
