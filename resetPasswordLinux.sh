cbe56b56f163e3880fada22987796b37d91bc125
shambhavi24 <shambhavi.shivan@yahoo.com>
2023-06-01 17:47:03 +0530

    Add files via upload

--- resetPasswordLinux.sh
+++ resetPasswordLinux.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Check if the script is being run as root
-if [[ $(id -u) -ne 0 ]]; then
-echo "This script must be run as root."
-exit 1
-fi
-
-# Check if a username is provided as an argument
-if [[ $# -ne 1 ]]; then
-echo "Usage: $0 "
-exit 1
-fi
-
 username=$1
 password=$2
 
@@ -23,3 +11,4 @@ echo "Password updated successfully for user: $username"
 else
 echo "Failed to update password for user: $username"
 fi
+exit 1
